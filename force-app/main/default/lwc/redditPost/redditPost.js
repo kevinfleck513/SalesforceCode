25d59c4eb3f757ef09498a8df7a6a419d2c4d5f3
kevinfleck513 <kfleck513@icloud.com>
2021-08-06 11:52:52 -0400

    code cleanup/remove unecessary variable

--- force-app/main/default/lwc/redditPost/redditPost.js
+++ force-app/main/default/lwc/redditPost/redditPost.js
@@ -3,12 +3,10 @@ import { LightningElement, api } from 'lwc';
 export default class RedditPost extends LightningElement {
 
  @api post;
- downs;
 
  connectedCallback(){
     console.log('connectedCallback() redditPost.js');
     console.log('post: ' + JSON.stringify(this.post));
-    // this.downs = this.post.downs = null ? 0 : this.post.downs;
  }
 
  get upOrDownArrow(){
